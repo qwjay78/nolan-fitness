f1c8b25101de05a7d27edb2f27777aa6c8a37f1d
joana <joana.lps.pro@gmail.com>
2024-03-14 13:26:52 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,4 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+
+
+  
 });
---
+++ scripts/formulaire-contact.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $nom_prenom = $_POST["nom_prenom"];
+    $adresse_mail = $_POST["adresse_mail"];
+    $message = $_POST["message"];
+
+    
+    $destinataire = "nolan@nolan-fitness.go.yj.fr";
+
+    // Sujet de l'e-mail
+    $sujet = "Nouveau message du formulaire de contact";
+
+    // Corps de l'e-mail
+    $corps_message = "Nom & Prénom: $nom_prenom\n";
+    $corps_message .= "Adresse e-mail: $adresse_mail\n\n";
+    $corps_message .= "Message:\n$message";
+
+    // En-têtes de l'e-mail
+    $headers = "De: $adresse_mail\r\n";
+    $headers .= "Répondre à: $adresse_mail\r\n";
+
+    
+    mail($destinataire, $sujet, $corps_message, $headers);
+
+    // Redirection 
+    header("Location: ../pages/merci.html");
+    exit();
+}
+?>
